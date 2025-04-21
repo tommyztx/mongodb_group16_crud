f3a4b160d864338bb2e5db35f97333d1391afd52
Tommy Zheng <tommyztx@gmail.com>
2025-04-21 15:50:48 -0500

    connect in db.js

--- db.js
+++ db.js
@@ -1,2 +1,35 @@
 // Connect to Mongo DB client, connect function
-const MongoClient = require("mongodb").MongoClient
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const dbname = "crud_mongodb";
+const url = "mongodb://localhost:27017";
+const mongoOptions = {useNewUrlPasser : true};
+
+const state = {
+    db : null
+};
+
+const connect = (cb) =>{
+    if(state.db)
+        (cb);
+    else {
+        MongoClient.connect(url,mongoOptions, (err,client)=>{
+            if(err) {
+                cb(err);
+            } else {
+                state.db = client.db(dbname);
+                cb();
+            }
+        });
+    }
+}
+
+const getPrimaryKey = (_id)=> {
+    return ObjectID(_id);
+}
+
+const getDB = ()=>{
+    return state.db;
+}
+
+module.exports = {getDB, connect, getPrimaryKey}
